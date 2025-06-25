d0c0b021368b3202d469f79bfc93fe7e8dcd1f5b
Emran Hossain <151853372+git-emran@users.noreply.github.com>
2025-06-25 22:05:00 +0600

    Showcase section 1

---
+++ src/sections/ShowcaseSection.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const ShowcaseSection = () => {
+  return (
+    <div id='work' className='app-showcase' >
+        <div className='w-full' >
+            <div className='showcaselayout' >
+                {/* LEFT */}
+                <div className='first-project-wrapper' >
+                    <div className='image-wrapper'  >
+                        <img src='/images/project1.png' alt='Ryde' />
+                    </div>
+
+                </div>
+
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default ShowcaseSection
