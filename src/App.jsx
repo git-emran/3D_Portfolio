4f0f9eec861cbc1fbba7dd71751e2b08a54410be
Emran Hossain <151853372+git-emran@users.noreply.github.com>
2025-06-27 00:03:58 +0600

    quick fixes

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Hero from "./sections/Hero";
 import ShowcaseSection from "./sections/ShowcaseSection";
 import Projects from "./sections/Projects";
 import Navbar from "./components/Navbar";
+import LogoSection from "./sections/LogoSection";
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
       <Hero />
       <ShowcaseSection />
       <Projects />
+      <LogoSection />
     </>
   );
 };
---
+++ src/sections/LogoSection.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const LogoSection = () => {
+  return (
+    <div className="md:my-20 my-10 relative">
+      <div />
+    </div>
+  );
+};
+
+export default LogoSection;
